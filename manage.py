440837249a07907d6b92dac5c478ccd87babdede
Michael Herman <hermanmu@gmail.com>
2024-07-12 08:15:16 -0500

    updates

--- manage.py
+++ manage.py
@@ -4,7 +4,9 @@
 import sys
 import unittest
 
+import redis
 from flask.cli import FlaskGroup
+from rq import Connection, Worker
 
 from project.server import create_app, db
 from project.server.models import User
@@ -36,6 +38,15 @@ def test():
         sys.exit(0)
     else:
         sys.exit(1)
+
+
+@cli.command('run_worker')
+def run_worker():
+    redis_url = app.config['REDIS_URL']
+    redis_connection = redis.from_url(redis_url)
+    with Connection(redis_connection):
+        worker = Worker(app.config['QUEUES'])
+        worker.work()
 
 
 if __name__ == '__main__':
---
+++ project/server/main/tasks.py
@@ -0,0 +1,33 @@
+# project/server/main/tasks.py
+
+
+import os
+
+import boto3
+
+from project.server import db
+from project.server.models import User
+
+
+def send_email(email, body):
+    # time.sleep(10)  # simulate long-running process
+    ses = boto3.client(
+        'ses',
+        region_name=os.getenv('SES_REGION'),
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+    )
+    ses.send_email(
+        Source=os.getenv('SES_EMAIL_SOURCE'),
+        Destination={'ToAddresses': [email]},
+        Message={
+            'Subject': {'Data': 'Confirm Your Account'},
+            'Body': {
+                'Text': {'Data': body}
+            }
+        }
+    )
+    user = User.query.filter_by(email=email).first()
+    user.email_sent = True
+    db.session.commit()
+    return True
